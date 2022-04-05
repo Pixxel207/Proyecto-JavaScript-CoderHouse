e5d3ffc00bb428dd1757b34c4b86b91dc9a86a45
pixxxel207 <ivanchu816@gmail.com>
2022-04-04 23:15:38 -0300

    Termino trabajo de librerias

--- index.js
+++ index.js
@@ -64,6 +64,52 @@ const productos = [{
     }
 
 ]
+
+const TopSelling = [
+    
+    {
+        id:1,
+        titulo:"Apple iPhone 13 Pro Max",
+        precio:150000,
+        stock:0,
+        precioSinRebaja:300000,
+        imagen:"https://nissei.com/media/catalog/product/cache/16a9529cefd63504739dab4fc3414065/i/p/iphone-13-pro-max-blue-selectyyyy.jpg"
+    },
+    {
+        id:2,
+        titulo:"Samsung Galaxy A51",
+        precio:150000,
+        stock:0,
+        precioSinRebaja:300000,
+        imagen:"https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTXi-S9ugudqDRV9vCybOhnGnkcR24GsqKRmPK3OoZuQJEfMsMmKVTTG2wrnJT3GUWg4zVPhhlj8gQyrHWbmRslPF8li2J41A"
+    },
+    {
+        id:3,
+        titulo:"Xiaomi Redmi 10",
+        precio:150000,
+        stock:0,
+        precioSinRebaja:300000,
+        imagen:"https://http2.mlstatic.com/D_NQ_NP_612869-MLA48741510265_012022-O.webp"
+    },
+    {
+        id:4,
+        titulo:" MOTOROLA EDGE 20 PRO",
+        precio:150000,
+        stock:0,
+        precioSinRebaja:300000,
+        imagen:"http://medias.musimundo.com/medias/00435014-144181-144181-01-144181-01.jpg-size515?context=bWFzdGVyfGltYWdlc3w3NTMxOHxpbWFnZS9qcGVnfGgwYS9oMjgvMTAzMDEzNjA5NjM2MTQvMDA0MzUwMTQtMTQ0MTgxLTE0NDE4MV8wMS0xNDQxODFfMDEuanBnX3NpemU1MTV8ZDQ0Mjc1ZTI3MDUxMzkxNzEzZjIyZjlhMzI0MGE3M2Q1ZjY2ZmM2Zjc1ZTgzMDNhMDE5OGE0YjU0NDA3YTExYg"
+    },
+    {
+        id:5,
+        titulo:"LG Velvet 128 Gb",
+        precio:150000,
+        stock:0,
+        precioSinRebaja:300000,
+        imagen:"https://http2.mlstatic.com/D_NQ_NP_844070-MLA46435469794_062021-O.webp"
+    }
+]
+
+
 // Funcion de agregar al carrito -----Comienzo-------
 // Funcion de agregar al carrito -----Comienzo-------
 
@@ -74,6 +120,7 @@ const agregarAlCarrito = (idProducto) => {
     carrito.push(productosAgregados);
     document.getElementById("cantidad-prod").innerHTML = carrito.length;
     localStorage.setItem("carrito", JSON.stringify(carrito));
+    swal("Agregaste el producto al carrito!", "Gracias por confiar!", "success");
 
 
 };
@@ -214,6 +261,40 @@ function mostrrarWidgetsEnElHtml(cards) {
 
 // Funcion generar widgets en el menu que despliega el carrito ------Fin-----
 // Funcion generar widgets en el menu que despliega el carrito ------Fin-----
+
+// Funcion para generar las cards en los productos del Top Sellings -----Inicio-----
+// Funcion para generar las cards en los productos del Top Sellings -----Inicio-----
+
+
+
+productosTopSellings();
+function productosTopSellings(){
+    let acumuladorDeCards = ` `;
+    TopSelling.forEach((producto) => {
+        acumuladorDeCards += `<div id="products-topSelling"class="product-widget">
+        <div class="product-img">
+            <img src="${producto.imagen}" alt="">
+        </div>
+        <div class="product-body">
+            <p class="product-category">Category</p>
+            <h3 class="product-name"><a href="#">${producto.titulo}</a></h3>
+            <h4 class="product-price">$${producto.precio}<del class="product-old-price">$990.00</del></h4>
+        </div>
+    </div>`
+    });
+    mostrarProductosTopSellingEnHtml(acumuladorDeCards);
+}
+function mostrarProductosTopSellingEnHtml(cards) {
+    document.getElementById("products-topSelling").innerHTML=cards;
+}
+
+
+
+
+// Funcion para generar las cards en los productos del Top Sellings -----Fin-----
+// Funcion para generar las cards en los productos del Top Sellings -----Fin-----
+
+
 
 // Funcion para poder sacar un producto de mi carrito  ----Inicio----
 // Funcion para poder sacar un producto de mi carrito  ----Inicio----
